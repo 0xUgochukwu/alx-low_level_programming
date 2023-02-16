9b9fb3d8fecb8760a915d68562ae7ebf5305277d
0xUgochukwu <0xUgochukwu@gmail.com>
2023-02-16 13:17:40 -0800

    writing to standard error file of the unix system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include<unistd.h>
+#include<string.h>
+
+/**
+ * main - This is the main Function
+ * Description: This function writes to the standard error output stream
+ * Return: It returns an error (1)`
+ */
+int main(void)
+{
+	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(STDERR_FILENO, text, sizeof(char) * strlen(text));
+
+	return (1);
+}
+
