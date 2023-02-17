2ecfc9cf12e98b522ed058e9eef1017b0de7e70c
0xUgochukwu <0xUgochukwu@gmail.com>
2023-02-17 15:11:09 -0800

    printing number combinations 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,38 +2,26 @@
 
 /**
  * main - the program entry point
- * Description: This function prints numbers
+ * Description: This function prints number combinations
  * Return: the function retuens zero
  */
 int main(void)
 {
 	int i, j;
-
-
+	
 	for (i = 0; i <= 99; i++)
 	{
 		for (j = i; j <= 99; j++)
 		{
 			if (i != j)
 			{
-				if (i < j)
-				{
-					putchar(i / 10 + '0');
-					putchar(i % 10 + '0');
-					putchar(' ');
-					putchar(j / 10 + '0');
-					putchar(j % 10 + '0');
-				}
-				else
-				{
-					putchar(j / 10 + '0');
-					putchar(j % 10 + '0');
-					putchar(' ');
-					putchar(i / 10 + '0');
-					putchar(i % 10 + '0');
-				}
-
-				if (i != 99 || j != 99)
+				putchar((i / 10) + '0');
+			       	putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				
+				if (i != 98 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');
@@ -41,6 +29,7 @@ int main(void)
 			}
 		}
 	}
+
 
 	putchar('\n');
 
