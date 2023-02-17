825d77d2d0d1f4707b73ec5bc1e1920955247dbc
0xUgochukwu <0xUgochukwu@gmail.com>
2023-02-17 12:55:07 -0800

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - the program entry point
+ * Description: This function prints alphabets
+ * Return: the function retuens zero
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+
+	for (j = 97; j < 10; j++)
+	{
+		putchar((char) j);
+	}
+
+	putchar('\n');
+
+
+	return (0);
+}
