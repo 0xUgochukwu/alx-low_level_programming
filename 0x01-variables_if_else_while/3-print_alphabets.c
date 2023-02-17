8f6524c95530ad1d6e4503079da28b7131231de8
0xUgochukwu <0xUgochukwu@gmail.com>
2023-02-17 12:18:05 -0800

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - the program entry point
+ * Description: This function prints alphabets
+ * Return: the function retuens zero
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar((char) i);
+	}
+
+	for (j = 65; i < 91; j++)
+	{
+		putchar((char) j);
+	}
+
+	putchar('\n');
+
+
+	return (0);
+}
