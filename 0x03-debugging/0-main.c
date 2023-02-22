e4023510b1e54c088bed21bbb01c7e82a45983ae
0xUgochukwu <0xUgochukwu@gmail.com>
2023-02-22 01:37:18 -0800

    + or -

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
-
 /**
- * main - tests if a number is positive or negative
- * Return: 0
- */
+  * main - Test function for positive or negative
+  * Return: 0
+  */
 
 int main(void)
 {
 	int i;
 
-	i = 98;
-
+	i = 0;
 	positive_or_negative(i);
 
-
-	return (0);
+return (0);
 }
