7730154b8f8ac727af629270367c0f5b53fd165a
0xUgochukwu <0xUgochukwu@gmail.com>
2023-02-23 11:01:22 -0800

    highest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - get the highest prime factor (hpf)
+ * of 612852475143
+ *
+ * Return: Always (0)
+ */
+
+
+int main(void)
+{
+	long int n = 612852475143;
+	long int hpf;
+
+	for (hpf = 2; hpf < x; hpf++)
+	{
+		if (x % hpf == 0)
+		{
+			x /= hpf;
+		}
+	}
+
+	printf("%ld\n", hpf);
+
+	return (0);
+}
