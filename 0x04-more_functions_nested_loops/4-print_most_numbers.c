7651b61c9ff2bf7476d8f968765b44146a04d130
0xUgochukwu <0xUgochukwu@gmail.com>
2023-02-23 08:34:36 -0800

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers between 0 to 9
+ * excluding 2 and 4
+ *
+ * Return: Void.
+ */
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i++);
+		}
+	}
+
+	_putchar('\n');
+}
