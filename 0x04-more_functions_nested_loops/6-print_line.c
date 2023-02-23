0b7f48d997cace53428dcaa21ea1c4a48f943d36
0xUgochukwu <0xUgochukwu@gmail.com>
2023-02-23 09:15:31 -0800

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * print_line - print a line in the terminal
+ * @n: length of the line to be printed
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		_putchar('_');
+		i++;
+	}
+
+	_putchar('$');
+	_putchar('\n');
+}
