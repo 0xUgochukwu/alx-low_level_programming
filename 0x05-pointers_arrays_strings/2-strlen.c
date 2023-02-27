a2d7173092d491348a417656e259d964fd715c4e
0xUgochukwu <0xUgochukwu@gmail.com>
2023-02-27 06:19:31 -0800

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+
+
+/**
+ * _strlen - get the length of a string
+ * @s: pointer to the string to be used
+ * Return: length of string
+ */
+
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
