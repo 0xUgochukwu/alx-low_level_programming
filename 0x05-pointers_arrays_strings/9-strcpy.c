2fb5c6726e0f3e2642d32cdbdd8d9e667069f94a
0xUgochukwu <0xUgochukwu@gmail.com>
2023-02-27 09:26:31 -0800

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include "main.h"
+
+
+/**
+ * _strcpy - copies a string to a buffer
+ * @src: pointer to string
+ * @dest: pointer to buffer
+ * Return: Pointer to copied string
+ */
+
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	
+	return (dest);
+}
