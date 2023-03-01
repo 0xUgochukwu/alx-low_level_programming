c37a6f7634375ba9b68e2ad834c7a0dd22ce31d1
0xUgochukwu <0xUgochukwu@gmail.com>
2023-03-01 14:44:31 -0800

    update betty style

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,6 +5,7 @@
  * _strcat - join two strings together
  * @dest: string to be joined to
  * @src: string to be joined
+ * Return: a pointer to the joined string
  */
 
 char *_strcat(char *dest, char *src)
