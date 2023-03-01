5232dcfab639362b64789c953e58921345719daa
0xUgochukwu <0xUgochukwu@gmail.com>
2023-03-01 14:42:55 -0800

    join strings together

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - join two strings together
+ * @dest: string to be joined to
+ * @src: string to be joined
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
