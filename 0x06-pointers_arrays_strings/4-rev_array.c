336c32ffaf0a364d27b4957e967c2128b57e776e
0xUgochukwu <0xUgochukwu@gmail.com>
2023-03-02 06:00:04 -0800

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,10 +12,10 @@ void reverse_array(int *a, int n)
 {
 	int temp, i = 0;
 
-	while (i < n)
+	while (i < (n / 2))
 	{
 		temp = a[i];
-		a[i] = a[n - i -1];
+		a[i] = a[n - i - 1];
 		a[n - i++ - 1] = temp;
 	}
 }
