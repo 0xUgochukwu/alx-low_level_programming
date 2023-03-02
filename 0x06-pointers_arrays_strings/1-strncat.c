1e8ff121a5e9261360bd6f929bcc16810a6910c2
0xUgochukwu <0xUgochukwu@gmail.com>
2023-03-02 03:03:54 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strncat - join two strings together
+ * @dest: string to be joined to
+ * @src: string to be joined
+ * @n: the function will use at most n bytes from src
+ * Return: a pointer to the joined string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	return (strcat(dest, src, n));
+}
