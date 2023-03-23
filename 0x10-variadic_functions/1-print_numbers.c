c52caf2d01fe1f0730f23a5d20a6acb17806b1d2
0xUgochukwu <0xUgochukwu@gmail.com>
2023-03-23 04:58:17 -0700

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,11 +10,13 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	if (seprator == NULL)
-		return;
 
 	va_list ap;
 	unsigned int i = 0;
+
+	if (separator == NULL)
+		return;
+
 	
 	va_start(ap, n);
 
