a26f6f91cf79b853721659f83ff4fd09a241fc13
0xUgochukwu <0xUgochukwu@gmail.com>
2023-03-24 04:17:16 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,28 +13,23 @@
  * Return: dog_t
  */
 dog_t *new_dog(char *name, float age, char *owner) {
-    // Allocate memory for the dog structure
     dog_t *dog = (dog_t *) malloc(sizeof(dog_t));
     if (dog == NULL) {
-        return NULL;  // Memory allocation failed
+        return NULL;  
     }
 
-    // Allocate memory for the name and owner strings
     dog->name = (char *) malloc(strlen(name) + 1);
     dog->owner = (char *) malloc(strlen(owner) + 1);
     if (dog->name == NULL || dog->owner == NULL) {
-        // Memory allocation failed
         free(dog->name);
         free(dog->owner);
         free(dog);
         return NULL;
     }
 
-    // Copy the name and owner strings
     strcpy(dog->name, name);
     strcpy(dog->owner, owner);
 
-    // Set the age of the dog
     dog->age = age;
 
     return dog;
