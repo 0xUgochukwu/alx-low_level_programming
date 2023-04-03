3a4240c5c99c9c5d8a1d7b699bd1a3e2b3cc8629
Ugochukwu Chukwuma <0xUgochukwu@gmail.com>
2023-04-03 12:13:34 +0100

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,14 +8,14 @@
 
 size_t list_len(const list_t *h)
 {
-	const list_t *temp;
+	
 	unsigned int counter = 0;
 
-	temp = h;
-	while (temp)
+	
+	while (h)
 	{
 		counter++;
-		temp = temp->next;
+		h = h.next;
 	}
 	return (counter);
 }
