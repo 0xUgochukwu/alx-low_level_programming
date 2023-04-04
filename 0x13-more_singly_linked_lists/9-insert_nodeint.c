43af7500df218af565ec0b1dabeea3399d5cedc8
0xUgochukwu <0xUgochukwu@gmail.com>
2023-04-04 15:37:43 -0700

    insert node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,6 +18,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	current = *head;
 
+	if (idx == 0)
+		return add_nodeint(head, n);
+
 	new = malloc(sizeof(listint_t));
 
 	if (new == NULL)
