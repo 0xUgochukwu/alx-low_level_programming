45936109aee769ef2b35ccb46e9169ee91896d7f
0xUgochukwu <0xUgochukwu@gmail.com>
2023-04-04 15:49:59 -0700

    insert node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,41 +8,45 @@
  * Return: the address of the new node, or NULL if it failed
  */
 
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *current, *new, *tmp;
+	listint_t *current, *new_node;
 	unsigned int i = 0;
 
 	if (head == NULL)
 		return (NULL);
 
-	new = malloc(sizeof(listint_t));
-
-	if (new == NULL)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
 		return (NULL);
+
+	new_node->n = n;
+
 	if (idx == 0)
 	{
-		new->next = *head;
-		*head = new;
-		return (new);
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
 
-	new->n = n;
 	current = *head;
 
-	while (current)
+	while (current != NULL && i < idx - 1)
 	{
-		if (idx - 1 == i)
-		{
-			tmp = current->next;
-			current->next = new;
-			new->next = tmp;
-			return (new);
-		}
-
 		current = current->next;
 		i++;
 	}
-	return (NULL);
+
+	if (current == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = current->next;
+	current->next = new_node;
+
+	return (new_node);
 }
 
