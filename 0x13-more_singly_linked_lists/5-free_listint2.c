bbcc8c37aa90228188a1dce293ae9b45d02d7c0b
0xUgochukwu <0xUgochukwu@gmail.com>
2023-04-04 14:25:29 -0700

    free list 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,19 +8,19 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *current, *next;
-	
+
 	if (head == NULL)
 		return;
-	
+
 	current = *head;
-	
+
 	while (current)
 	{
 		next = current->next;
 		free(current);
 		current = next;
 	}
-	
+
 	*head = NULL;
 }
 
