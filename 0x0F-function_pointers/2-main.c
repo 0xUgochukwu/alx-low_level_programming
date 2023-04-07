1db5f52d5b9568bd932c0b7f7bfbfcaa07536628
Ugochukwu Chukwuma <ceeugochukwu@gmail.com>
2023-04-07 22:44:32 +0100

    Update 2-main.c

--- 0x0F-function_pointers/2-main.c
+++ 0x0F-function_pointers/2-main.c
@@ -5,7 +5,7 @@
  * is_98 - check if a number is equal to 98
  * @elem: the integer to check
  *
- * Return: 0 if false, something else otherwise.
+ * Return: 0 if false, something else if otherwise.
  */
 int is_98(int elem)
 {
@@ -16,7 +16,7 @@ int is_98(int elem)
  * is_strictly_positive - check if a number is greater than 0
  * @elem: the integer to check
  *
- * Return: 0 if false, something else otherwise.
+ * Return: 0 if false, something else if otherwise.
  */
 int is_strictly_positive(int elem)
 {
