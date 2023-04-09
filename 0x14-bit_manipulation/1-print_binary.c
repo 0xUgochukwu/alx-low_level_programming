e57cf1adf2b7f20626f795d25985c387a666fae2
0xUgochukwu <0xUgochukwu@gmail.com>
2023-04-09 12:10:53 -0700

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_binary - print the binaray of a given integer
+ * @n: given integer
+ */
+
+
+void print_binary(unsigned long int n)
+{
+	int i = 63, start = 0;
+	unsigned long int current;
+
+	while (i <= 0)
+	{
+		current = n >> i--;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			start++;
+		}
+		else if (start)
+			_putchar('0');
+	}
+
+	if (!start)
+		_putchar('0');
+
+}
