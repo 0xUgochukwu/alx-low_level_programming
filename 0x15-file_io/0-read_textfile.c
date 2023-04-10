95c8599e3cf35974cbd30d2bb38690b3d721ec89
0xUgochukwu <0xUgochukwu@gmail.com>
2023-04-10 09:02:30 -0700

    read file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * read_textfile - read a text file
+ * @filename: name of file to read
+ * @letters: number of letters to read
+ * Return: number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char c;
+	size_t i = 0;
+	FILE *fp;
+
+	if (filename == NULL)
+		return (0);
+	
+	fp = fopen(filename, "r");
+
+	if (fp == NULL)
+		return (0);
+
+	while (i < letters && (c = getc(fp)) != EOF)
+	{
+		if (write(STDOUT_FILENO, &c, 1) != 1)
+			return (0);
+		
+		i++;
+	}
+
+	fclose(fp);
+
+	return (i);
+}
