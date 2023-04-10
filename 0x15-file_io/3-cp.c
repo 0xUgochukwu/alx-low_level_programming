b808cb83943e5d1a96d9c6dd5afc51be6dc2ec53
0xUgochukwu <0xUgochukwu@gmail.com>
2023-04-10 12:41:28 -0700

    copy from one file into another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -42,7 +42,7 @@ int main(int ac, char **av)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			close(src_fd);
-			close(des_fd);
+			close(dest_fd);
 			return (99);
 		}
 	}
