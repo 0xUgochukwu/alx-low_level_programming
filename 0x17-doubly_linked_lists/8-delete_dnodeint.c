e5bd89398d5a1d9ce840475c8281473ade6cf740
0xUgochukwu <0xUgochukwu@gmail.com>
2023-05-18 07:39:02 -0700

    delete works ffs

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,6 +11,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i = 0;
 	dlistint_t *current = *head;
 
+	if (*head == NULL)
+		return (-1);
+
 	if (index == 0)
 	{
 		*head = (*head)->next;
