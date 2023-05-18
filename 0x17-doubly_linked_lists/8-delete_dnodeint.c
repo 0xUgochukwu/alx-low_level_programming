d6ca96685f7d7b6784b3f99dbb6b9058766ce420
0xUgochukwu <0xUgochukwu@gmail.com>
2023-05-18 07:12:44 -0700

    delete

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,9 +12,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i = 0;
 	dlistint_t *current = *head;
 
-	if (idx == 0)
+	if (index == 0)
 	{
-		*head = head->next;
+		*head = (*head)->next;
 		free((*head)->prev);
 		(*head)->prev = NULL;
 
@@ -23,14 +23,14 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	while (current)
 	{
-		if (current->next == NULL && i == idx - 1)
+		if (current->next == NULL && i == index - 1)
 		{
 			free(current);
 			current->prev = NULL;
 			return (1);
 		}
 
-		if (i == idx - 1)
+		if (i == index - 1)
 		{
 			free(current->next);
 			current->next = current->next->next;
